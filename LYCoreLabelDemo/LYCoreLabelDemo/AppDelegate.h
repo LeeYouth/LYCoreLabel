054c8706336503b01cf766a0aabfcce928f7cd12
LeeYouth <yuanliyong520@126.com>
2015-09-24 15:56:55 +0800

    first commit

---
+++ LYCoreLabelDemo/LYCoreLabelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYCoreLabelDemo
+//
+//  Created by LYoung on 15/9/24.
+//  Copyright (c) 2015年 LYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYCoreLabelDemo/LYCoreLabelDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYCoreLabelDemo
+//
+//  Created by LYoung on 15/9/24.
+//  Copyright (c) 2015年 LYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
